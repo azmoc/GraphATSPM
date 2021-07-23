0959bd61dd3f850a09ec5bdec0c4a963f0266383
azmoc <dcomsa@byu.edu>
2021-07-23 06:12:13 -0600

    added table to plot

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # GraphATSPM
 
 This program turns a graph of traffic volume into data values. The UDOT ATSPM website provides graphs of traffic metrics but does not provide the data used to create the graphs. One such useful metric is traffic volume for an intersection. The UDOT ATSPM website provides a jpeg of the graph. This code reads it and saves a CSV file in an existing folder called "Values/".
+The graph must at least and only show northbound and southbound traffic volumes.
 Can take arguments of jpegs' relative file paths
-Can take argument "show" to also display comparison graph of extracted values.
+Can take argument "show" to also display comparison graph and table of extracted values.
+Can take argument "save" to also save jpeg of comparison graph and table.
+Some parameters may need adjustment depending on the graph.
--- graphToCSV.py
+++ graphToCSV.py
@@ -5,6 +5,7 @@ ATSPM volume approach graphs.
 Convert the jpeg to rgb arrays and then find the vertex in that column
 """
 
+from matplotlib.transforms import Bbox
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
@@ -12,25 +13,26 @@ import sys
 
 
 def Booleans(args, file_names):
-    show_graphs = False
     output = True
     output_month = False
+    show_graphs = False
+    save_graphs = False
     # Get list of file_names and update boolean
     if len(args) > 0:
         print(f"\nArguments of the script : {args}")
         if 'show' in args:
             show_graphs = True
             args.remove('show')
+        if 'save' in args:
+            save_graphs = True
+            args.remove('save')
         # To keep reference, use +=
         file_names += args
     if len(file_names) == 0:
         if not show_graphs:
             output_month = True
         output = False
-        # Test single graphs
-        # file_names += [r"Graphs\VolumesGraph2020-05-28.jpg"]
-        # file_names += ["Graphs\\testVolumes2019-10-01.jpg"]
-    return show_graphs, output, output_month
+    return (output, output_month, show_graphs, save_graphs)
 
 
 def GetBox(color_band):
@@ -51,13 +53,10 @@ def GetBox(color_band):
 def GetVal(q, color_band, box_top, box_bot, grid_height, shift_row_inds, scaling):
     # Look at column slice width of two pixels, height of grid + 2 extra pixels
     pixel_columns = color_band[box_top: box_bot + 3, q: q+2].astype(float)[::-1]
-    # White out the grid lines, 2 pixel offset from bottom
+    # White out the grid lines, 2 pixel offset from bottom, edit value 190 as needed
     pixel_columns[grid_height - shift_row_inds + 2] = 190
     # Average between this pixel and the next one up
     k_av = np.array([np.sum(pixel_columns[pix:pix + 2]/2) for pix in range(grid_height)])#[:-1]
-    # if np.argmin(k_av) > grid_height-2:
-    #     print(k[-40:])
-    #     print(k_av[-40:])
     # A 3x3 grid has its center in middle pixel, 2x2 has center between pixels
     # Offset by 0.5 - 2
     return (np.argmin(k_av)-1.5)*scaling
@@ -74,13 +73,6 @@ def GenGraphVals(color_band, row_grid_inds, col_grid_inds, scaling, is_red):
     grid_height = box_bot - box_top
     # Shifted indices start at 0
     shift_row_inds = row_grid_inds - box_top
-    # Check output for expected values
-    # if not is_red:
-    #     temp = col_grid_inds[10] + 16
-    #     pixel_column = color_band[box_top:box_bot, temp:temp+2]
-    #     print(np.array([(i,row) for i,row in enumerate(pixel_column)]))
-    #     print(np.array([(i,row) for i, row in enumerate([np.sum(pixel_column[pix:pix+2]/2) for pix in range(grid_height)]) ]))
-    #     pass
     # Divide each grid square into 4 columns where to sample data
     # Slow and exact
     # quarters = np.concatenate([ np.linspace(c, col_grid_inds[i+1], 5)[:-1] for i,c in enumerate(col_grid_inds[:-1]) ]).astype(int)
@@ -106,9 +98,6 @@ def GenGraphVals(color_band, row_grid_inds, col_grid_inds, scaling, is_red):
         m = ( (vals[i*2 + 1] - vals[i*2]) / (quarters[i+1]-1.5 - (quarters[i]+1.5)) )
         yield max(0, (temp + vals[i*2] - 1.5 * m) / 2)
         temp = vals[i*2 + 1] + 1.5 * m
-        # print(f'{(i+1)/4},{temp:.2f}')
-        # if (i+1)/4 == 12.25:
-        #     print(f'quarter: {quarters[i+1]}, col: {col_grid_inds[12]}')
     yield max(0, temp)
 
 
@@ -116,7 +105,7 @@ def GenGraphVals(color_band, row_grid_inds, col_grid_inds, scaling, is_red):
 
 if __name__ == "__main__":
     file_names = []
-    show_graphs, output, output_month = Booleans(sys.argv[1:], file_names)
+    output, output_month, show_graphs, save_graphs = Booleans(sys.argv[1:], file_names)
     if not output:
         # Default compile month worth of volumes, default January 2020
         MONTH_DAY_COUNTS = [31,29,31,30,31,30,31,31,30,31,30,31]
@@ -146,30 +135,12 @@ if __name__ == "__main__":
         if show_graphs:
             graph_region_im = im.crop(box)
             graph_region_im.show()
-        # Show each color band of graph
-        # graph_region_rim = rim.crop(box)
-        # graph_region_rim.show()
-        # graph_region_gim = gim.crop(box)
-        # graph_region_gim.show()
-        # graph_region_bim = bim.crop(box)
-        # graph_region_bim.show()
 
         # Returns numpy array
         r_scaled_vals = GenGraphVals(r, row_grid_inds, col_grid_inds, scaling, is_red=True)
         b_scaled_vals = GenGraphVals(b, row_grid_inds, col_grid_inds, scaling, is_red=False)
         r_scaled_vals = tuple(r_scaled_vals)
         b_scaled_vals = tuple(b_scaled_vals)
-
-        # If need to check for out of bounds pixels
-        # r_scaled_vals = tuple(r_scaled_vals)
-        # b_scaled_vals = tuple(b_scaled_vals)
-        # Pixel 0 is out of bounds
-        # if np.count_nonzero(r_scaled_vals) != len(r_scaled_vals):
-        #     print(f'r_scaled_vals non pos: {r_scaled_vals}')
-        # if np.count_nonzero(b_scaled_vals) != len(b_scaled_vals):
-        #     print(f'b_scaled_vals non pos: {b_scaled_vals}')
-        # Check scaled values
-        # print(f'rlen:{len(r_scaled_vals)},blen:{len(b_scaled_vals)},xlen:{len(x_scaled)}')
         
         # Compile month worth of values
         if output_month:
@@ -178,47 +149,51 @@ if __name__ == "__main__":
 
         # Output CSV file for one graph
         if output:
-            outputname = f"TEST\\TEST_Volumes{name[-14:-4]}.csv"
-            # outputname = f"Volumes\\Volumes{name[-14:-4]}.csv"
+            # outputname = f"TEST\\TEST_Volumes{name[-14:-4]}.csv"
+            outputname = f"Volumes\\Volumes{name[-14:-4]}.csv"
             with open(outputname, 'w') as csv_f:
                 csv_f.write("Date,Hour,Northbound Volume,Southbound Volume")
                 csv_f.writelines([f'\n1/{int(time // 24 + 1)}/2020,{time%24},{int(round(n_vol))},{int(round(s_vol))}' for time,n_vol,s_vol in zip(x_scaled[:-1],r_scaled_vals,b_scaled_vals)])
         
-        if show_graphs:
+        if show_graphs or save_graphs:
             ratio = (col_grid_inds[-1] - col_grid_inds[0]) / (row_grid_inds[-1] - row_grid_inds[0])
             # 5 inches height is good size
-            fig = plt.figure(figsize=[ratio * 5, 5])
+            fig = plt.figure(figsize=[ratio * 5, 25/3])
             ax = fig.gca()
-            ax.set_xticks(x_scaled[::4])
-            ax.set_yticks(np.linspace(0, rows_minus_one * 200, rows_minus_one + 1))
-            # Tight around the box
-            ax.set_xlim([x_scaled[0], x_scaled[-1]])
-            ax.set_ylim([0, rows_minus_one * 200])
             plt.plot(x_scaled[:-1], tuple(r_scaled_vals))
             plt.plot(x_scaled[:-1], tuple(b_scaled_vals))
             plt.grid()
-            plt.show()
-            # Save the plot figure
-            # plt.savefig(f'Plots\\extracted{name[-14:-4]}.jpg')
+            # https://matplotlib.org/stable/gallery/misc/table_demo.html#sphx-glr-gallery-misc-table-demo-py
+            rows = [f':{i:02}' for i in range(0,60,15)] * 2
+            cell_text = [[f'{int(round(r))}' for r in r_scaled_vals[i::4]] for i in range(4)] + [[f'{int(round(b))}' for b in b_scaled_vals[i::4]] for i in range(4)]
+            colors = [[0,0,1,.2]]*4 + [[1,0,0,.2]]*4
+            the_table = plt.table(cellText = cell_text,
+                                  rowLabels=rows,
+                                  rowColours= colors,
+                                #   colLabels=x_scaled[:-1:4].astype(int),
+                                  bbox=[0, -2/3, 1, 16/27],
+                                  loc='bottom')
+            plt.subplots_adjust(left=0.1, bottom=0.4)
+            # Set x and y
+            ax.set_xticks(x_scaled[::4])
+            ax.set_yticks(np.linspace(0, rows_minus_one * 200, rows_minus_one + 1))
+            # Tight around the graphing box
+            ax.set_xlim([x_scaled[0], x_scaled[-1]])
+            ax.set_ylim([0, rows_minus_one * 200])
+            # Save or show the plot figure
+            if save_graphs:
+                # plt.savefig(f'Plots\\TEST_extracted{name[-14:-4]}.jpg')
+                plt.savefig(f'Plots\\extracted{name[-14:-4]}.jpg')
+            else:
+                plt.show()
 
     # Output CSV file for one month of graphs
     if output_month:
-        # Use generators for speed
-        # count = 0
-        # for sublist in north_v:
-        #     icount = count
-        #     for item in sublist:
-        #         icount += 1
-        #         print(item)
-        #         if icount > 5:
-        #             break
-        #     count += 1
-        #     if count > 5:
-        #         break
+        # Use generators for memory efficiency
         north_v_gen = (item for sublist in north_v for item in sublist)
         south_v_gen = (item for sublist in south_v for item in sublist)
-        outputname = f"TEST\\TEST_{signalID}Volumes" + file_base[-8:-1] + ".csv"
-        # outputname = f"Volumes\\{signalID}Volumes" + file_base[-8:-1] + ".csv"
+        # outputname = f"TEST\\TEST_{signalID}Volumes" + file_base[-8:-1] + ".csv"
+        outputname = f"Volumes\\{signalID}Volumes" + file_base[-8:-1] + ".csv"
         timestamps = np.arange(0, 24 * month_days, .25)
         with open(outputname, 'w') as csv_f:
             csv_f.write("Date,Hour,Northbound Volume,Southbound Volume")
